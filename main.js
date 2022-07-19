a13594870fdcc5cea8de6d8eea7bc656767dcabc
Hady_Muhammed <hadyymohammed@gmail.com>
2022-07-19 23:36:02 +0300

    Enhanced Design

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let contactBoxL = document.getElementById("left");
 let contactBoxR = document.getElementById("right");
+let skillsTOP = document.getElementById("top");
+let skillsBOTTOM = document.getElementById("bottom");
 window.onscroll = function () {
     if (window.scrollY >= 3729) {
         contactBoxL.style.animation = "comingleft 1s ease-in 1";
@@ -7,5 +9,9 @@ window.onscroll = function () {
     } else {
         contactBoxL.style.animation = "";
         contactBoxR.style.animation = ""; 
+    }
+    if (window.scrollY >= 663 && window.scrollY <= 1836) {
+        skillsTOP.style.animation = "comingUP 1s ease-in 1";
+        skillsBOTTOM.style.animation = "comingDOWN 1s ease-in 1"; 
     }
 }

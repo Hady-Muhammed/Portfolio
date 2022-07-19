7d1421c818858b5c7741401f19e0fa009d65c96f
Hady_Muhammed <hadyymohammed@gmail.com>
2022-07-19 23:27:26 +0300

    Enhanced Design

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let contactBoxL = document.getElementById("left");
 let contactBoxR = document.getElementById("right");
 window.onscroll = function () {
-    if (window.scrollY >= 2529) {
+    if (window.scrollY >= 3729) {
         contactBoxL.style.animation = "comingleft 1s ease-in 1";
         contactBoxR.style.animation = "comingright 1s ease-in 1"; 
     } else {

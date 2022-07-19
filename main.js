98c7a64547b35b1d76552b0db263976acdc8e6b6
Hady_Muhammed <hadyymohammed@gmail.com>
2022-07-20 00:21:20 +0300

    Enhanced Design

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let contactBoxL = document.getElementById("left");
 let contactBoxR = document.getElementById("right");
 let skillsTOP = document.getElementById("top");
 let skillsBOTTOM = document.getElementById("bottom");
+let cards = document.querySelectorAll(".col-lg-4");
 window.onscroll = function () {
     if (window.scrollY >= 3729) {
         contactBoxL.style.animation = "comingleft 1s ease-in 1";
@@ -11,10 +12,19 @@ window.onscroll = function () {
         contactBoxR.style.animation = ""; 
     }
     if (window.scrollY >= 663 && window.scrollY <= 1836) {
-        skillsTOP.style.animation = "comingUP 1s ease-in 1";
-        skillsBOTTOM.style.animation = "comingDOWN 1s ease-in 1"; 
+        skillsTOP.style.animation = "comingUP .5s ease-in 1";
+        skillsBOTTOM.style.animation = "comingDOWN .5s ease-in 1"; 
     } else {
         skillsTOP.style.animation = "";
         skillsBOTTOM.style.animation = ""; 
+    }
+    if (window.scrollY >= 167 && window.scrollY <= 1404) {
+        for(let i = 0 ; i < cards.length ; i++){
+            cards[i].style.animation = "skeww 1s ease-in 1";
+        }
+    } else {
+        for(let i = 0 ; i < cards.length ; i++){
+            cards[i].style.animation = "";
+        }
     }
 }

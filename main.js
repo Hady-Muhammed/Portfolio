43e4231cdf6034358f8c8401ec3235752d097565
Hady_Muhammed <hadyymohammed@gmail.com>
2022-08-11 15:26:06 +0300

    Improvements

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 let contactBoxL = document.getElementById("left");
 let contactBoxR = document.getElementById("right");
-let skillsTOP = document.getElementById("top");
-let skillsBOTTOM = document.getElementById("bottom");
-let cards = document.querySelectorAll(".col-lg-4");
 let nav = document.getElementById("nav");
 let contactTabs = document.querySelectorAll(".contact .box");
+let aos = document.querySelectorAll('.aos');
 window.onscroll = function () {
     if (window.scrollY >= 4441) {
         contactBoxL.style.animation = "comingleft 1s ease-in 1";
@@ -13,21 +11,20 @@ window.onscroll = function () {
         contactBoxL.style.animation = "";
         contactBoxR.style.animation = ""; 
     }
-  
-    if (window.scrollY >= 167 && window.scrollY <= 1404) {
-        for(let i = 0 ; i < cards.length ; i++){
-            cards[i].style.animation = "skeww .5s ease-in 1";
-        }
-    } else {
-        for(let i = 0 ; i < cards.length ; i++){
-            cards[i].style.animation = "";
-        }      
-    }
+    // Nav Bar
     if (window.scrollY >= 900 && window.scrollY <= 2000) {
         nav.classList.add("bg-dark");
     } else {
         nav.classList.remove("bg-dark");
     }
+    // AOS For Service Boxes
+    const triggerBottom = (window.innerHeight / 5) * 5;
+    aos.forEach((box) => {
+        const boxTop = box.getBoundingClientRect().top;
+        if (boxTop < triggerBottom) {
+            box.style.animation = 'skeww .5s ease-in 1'
+        }
+    });
 }
 
 contactTabs[0].onclick = function () {

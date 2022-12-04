9ff797a72142b9f89efed12729e96258fd1dbdbd
Hady_Muhammed <hadyymohammed@gmail.com>
2022-12-04 16:57:54 +0200

    added Skills and updated cv

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 let contactBoxL = document.getElementById("left");
 let contactBoxR = document.getElementById("right");
 let nav = document.getElementById("nav");
-let contactTabs = document.querySelectorAll(".contact .box");
-let aos = document.querySelector(".aos");
 let contact = document.querySelector(".contact");
 let serviceSection = document.getElementById("services");
 let projectSection = document.getElementById("projects");
@@ -12,6 +10,7 @@ let modalTxt = document.querySelector('.modal-text')
 let root = document.querySelector(':root');
 let landing = document.querySelector('section');
 let loading = document.querySelector('.loading')
+
 window.onscroll = function () {
   if (window.scrollY >= contact.offsetTop - 700) {
     contactBoxL.style.animation = "comingleft 1s ease-in 1";
@@ -32,20 +31,9 @@ window.onscroll = function () {
   ) {
     nav.classList.remove("bg-dark");
   }
-  // AOS For Service Boxes
-  if (window.scrollY >= aos.offsetTop - 900) {
-    aos.style.animation = "skeww .5s ease-in 1";
-  } else {
-    aos.style.animation = "";
-  }
 };
 
-contactTabs[0].onclick = function () {
-  window.location.href = "tel:+01283861152";
-};
-contactTabs[1].onclick = function () {
-  window.location.href = "mailto:hadyymohammed@gmail.com";
-};
+
 
 
 themeTabs.forEach((tab) => {
@@ -57,7 +45,6 @@ themeTabs.forEach((tab) => {
                 root.style.setProperty("--theme-color3","#e91e6378")
                 root.style.setProperty("--theme-color4","#e91e6378")
                 landing.style.setProperty("background-image","url('https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&cs=tinysrgb&w=1400')")
-                // landing.style.backgroundImage = "url(../images/landing1.jpg)";
             }
             else if (tab.id === 'theme2'){
                 root.style.setProperty("--theme-color1","#03a9f4") 
@@ -65,7 +52,6 @@ themeTabs.forEach((tab) => {
                 root.style.setProperty("--theme-color3","#03a9f4")
                 root.style.setProperty("--theme-color4","#03a9f4")
                 landing.style.setProperty("background-image","url('https://images.pexels.com/photos/237272/pexels-photo-237272.jpeg?auto=compress&cs=tinysrgb&w=1400')")
-                // landing.style.backgroundImage = "url(../images/landing2.jpg)"
             } 
             else if (tab.id === 'theme3') {
                 root.style.setProperty("--theme-color1","green") 
@@ -73,7 +59,6 @@ themeTabs.forEach((tab) => {
                 root.style.setProperty("--theme-color3","seagreen")
                 root.style.setProperty("--theme-color4","#198754")
                 landing.style.setProperty("background-image","url('https://images.pexels.com/photos/3374205/pexels-photo-3374205.jpeg?auto=compress&cs=tinysrgb&w=1400&h=750&dpr=1')")
-                // landing.style.backgroundImage = "url(../images/landing3.jpg)"
                 localStorage.setItem("color","green");
                 localStorage.setItem("color2","#2ad284");
                 localStorage.setItem("color3","seagreen");

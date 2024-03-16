b9aa3ac226255e716d47c6ee94e7fffa228532e5
Hady Muhammed <hadyymohammed@gmail.com>
2024-03-16 17:18:19 +0200

    adding tooltips and svg skills icons

--- main.js
+++ main.js
@@ -95,3 +95,10 @@ setTimeout(() => {
     loading.remove();
   }, 1000);
 }, 2000);
+
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
